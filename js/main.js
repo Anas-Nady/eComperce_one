8126e62a9218a344cb3ba27bfd84be4649e637e7
Anas-Nady <eng.anas.nady@gmail.com>
2022-04-15 15:08:06 +0200

    Create Project eComperce

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+// $(function(){
+
+//     $('body').css("paddingTop", $("nav").innerHeight())
+
+// });
+
+
+// btn Scroll 
+// window.onscroll = function() {
+
+//     if (window.pageYOffset >= 800) {
+//     btnScroll.style.display = 'block'
+//     }
+//     else{
+//     btnScroll.style.display = 'none'
+//     }
+
+// }
+
+// btnScroll.onclick = function() {
+//     window.scrollTo({
+//         top:0,
+//         left:0,
+//     })
+// }
+
+
+// Menu Toggle navbar 
+
+let menuItems = document.getElementById('menuItem')
+
+menuItems.style.maxHeight = '0px';
+
+function menuToggle() {
+    if(menuItems.style.maxHeight == '0px') {
+        menuItems.style.maxHeight = '200px'
+    }else{
+        menuItems.style.maxHeight = '0px';
+    }
+}
+
+
+
+// ----- Product details page 
+
+
+
+
+
+
+// Form Toggle 
+
+
